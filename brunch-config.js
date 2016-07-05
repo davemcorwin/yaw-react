facbee852dfe4a2d654b789f8cedaabfe41c7de9
David Corwin <dave@launchpadlab.com>
2016-07-05 08:50:05 -0500

    initial commit

---
+++ brunch-config.js
@@ -0,0 +1,17 @@
+exports.config = {
+  hot: true,
+
+  files: {
+    javascripts: { joinTo: 'app.js' },
+    stylesheets: { joinTo: 'app.css' }
+  },
+
+  plugins: {
+    babel: { presets: ['es2015', 'react', 'stage-0'] },
+    postcss: {
+      processors: [
+        require('autoprefixer')(['last 4 versions'])
+      ]
+    }
+  }
+};
